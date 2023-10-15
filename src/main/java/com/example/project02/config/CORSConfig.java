b7d21529f73adb4cb551ccaa756eec517266a4a2
wsb347 <wsb347@naver.com>
2023-10-15 22:23:20 +0900

    UPDATE CORS

--- src/main/java/com/example/project02/config/CORSConfig.java
+++ src/main/java/com/example/project02/config/CORSConfig.java
@@ -11,6 +11,7 @@ public class CORSConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // 모든 요청에 대해서
                 .allowedOrigins("http://localhost:3000") // 이 오리진에서 오는 요청만 허용
+                .allowedOriginPatterns("*") //모든 출처에서의 요청 허용
                 .allowedMethods("*") // 모든 HTTP 메소드 허용
                 .allowCredentials(true) // 쿠키 전달 허용
                 .allowedHeaders("*") // 모든 헤더 허용
