ef84642e33d4819fd2f80597b851f86b3a3a5d54
장근영 <jky00914@naver.com>
2023-10-17 23:58:46 +0900

    CORS

--- src/main/java/com/example/project02/config/CORSConfig.java
+++ src/main/java/com/example/project02/config/CORSConfig.java
@@ -10,8 +10,7 @@ public class CORSConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // 모든 요청에 대해서
-//                .allowedOrigins("http://localhost:3000") // 이 오리진에서 오는 요청만 허용
-                .allowedOriginPatterns("*")
+                .allowedOrigins("http://localhost:3000") // 이 오리진에서 오는 요청만 허용
                 .allowedMethods("*") // 모든 HTTP 메소드 허용
                 .allowCredentials(true) // 쿠키 전달 허용
                 .allowedHeaders("*") // 모든 헤더 허용
